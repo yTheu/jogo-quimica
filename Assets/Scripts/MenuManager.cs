92d8a16ec909d594ea74152e7185c801814d2f7b
matheus <matheuscs0v7@gmail.com>
2026-09-15 17:02:49 -0300

    tela final

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,38 +3,43 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    [Header("Menu inicial")]
     [SerializeField] private GameObject howToPlayPanel;
+
+    [Header("Cenas")]
+    [SerializeField] private string menuSceneName = "Menu";
+    [SerializeField] private string firstLevelSceneName = "Temperatura";
 
     private void Start()
     {
         Time.timeScale = 1f;
 
         if (howToPlayPanel != null)
-        {
             howToPlayPanel.SetActive(false);
-        }
     }
 
     public void StartGame()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene("Temperatura");
+        SceneManager.LoadScene(firstLevelSceneName);
+    }
+
+    public void ReturnToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(menuSceneName);
     }
 
     public void ShowHowToPlay()
     {
         if (howToPlayPanel != null)
-        {
             howToPlayPanel.SetActive(true);
-        }
     }
 
     public void HideHowToPlay()
     {
         if (howToPlayPanel != null)
-        {
             howToPlayPanel.SetActive(false);
-        }
     }
 
     public void ExitGame()
